01d8912dac815783be97a66d6cb0e6187a2e2065
mahisha-mudaliar <mahishamudaliar2603@gmail.com>
2024-01-05 23:46:46 +0530

    third

--- CardsinReact2/src/components/Cards.tsx
+++ CardsinReact2/src/components/Cards.tsx
@@ -2,7 +2,7 @@ import Card from 'react-bootstrap/Card';
 
 function Cards() {
   return (
-    <Card style={{ width: '18rem', backgroundColor: "grey" }}>
+    <Card style={{ width: '50rem', backgroundColor:'white', borderRadius:'2rem'}}>
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>

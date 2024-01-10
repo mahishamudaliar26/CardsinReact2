eebdf8886182e03221078ba214054fb1e33eda3d
mahisha-mudaliar <mahishamudaliar2603@gmail.com>
2024-01-10 23:20:10 +0530

    TableEg

--- CardsinReact2/src/App.tsx
+++ CardsinReact2/src/App.tsx
@@ -1,19 +1,19 @@
 import { useState } from 'react'
-import DataTable from 'react-data-table-component';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Cards from './components/Cards'
 import PaginationBasic from './components/Pagination';
+import TablesEg from './components/TablesEg';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Cards/>
-      <PaginationBasic/>
-      
+      {/* <Cards/>
+      <PaginationBasic/> */}
+      <TablesEg/>
     </>
   )
 }
